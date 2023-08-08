3e0647872546b7ed03047c67a07d21d14b51475f
Kevin Bond <kevinbond@gmail.com>
2023-08-08 16:13:07 -0400

    remove `SnapshotCommand`

--- config/storage_orm.php
+++ config/storage_orm.php
@@ -8,7 +8,6 @@ use Symfony\Component\Messenger\Event\WorkerMessageHandledEvent;
 use Symfony\Component\Messenger\Event\WorkerMessageReceivedEvent;
 use Zenstruck\Messenger\Monitor\Command\PurgeCommand;
 use Zenstruck\Messenger\Monitor\Command\SchedulePurgeCommand;
-use Zenstruck\Messenger\Monitor\Command\SnapshotCommand;
 use Zenstruck\Messenger\Monitor\History\HistoryListener;
 use Zenstruck\Messenger\Monitor\History\ResultNormalizer;
 use Zenstruck\Messenger\Monitor\History\Storage;
@@ -39,13 +38,6 @@ return static function (ContainerConfigurator $container): void {
             ->tag('kernel.event_listener', ['method' => 'receiveMessage', 'event' => WorkerMessageReceivedEvent::class])
             ->tag('kernel.event_listener', ['method' => 'handleSuccess', 'event' => WorkerMessageHandledEvent::class])
             ->tag('kernel.event_listener', ['method' => 'handleFailure', 'event' => WorkerMessageFailedEvent::class])
-
-        ->set('.zenstruck_messenger_monitor.command.snapshot', SnapshotCommand::class)
-            ->args([
-                service('zenstruck_messenger_monitor.history.storage'),
-                service('zenstruck_messenger_monitor.transport_monitor'),
-            ])
-            ->tag('console.command')
 
         ->set('.zenstruck_messenger_monitor.command.purge', PurgeCommand::class)
             ->args([
