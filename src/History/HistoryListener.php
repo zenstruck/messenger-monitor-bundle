992fb87dafd5c41c8591a9f22e9240112a6dece1
Martin Vlcek <1829574+dontfreakout@users.noreply.github.com>
2024-02-16 16:10:45 +0100

    feat: Add parameter to DisableMonitoringStamp to disable only when message has no handler (#68)
    * [FTR] Add optional handler restriction to DisableMonitoringStamp
    
    Now, the `DisableMonitoringStamp` class includes a boolean constructor parameter that determines whether monitoring should only be disabled when there are no handlers. The `isMonitoringDisabled` method in `HistoryListener` has been updated to consider this new setting. Tests were extended to verify the correct behavior for both situations.
    
    * Add instructions to disable monitoring of handler-less messages
    
    * [REF] Enhance DisableMonitoringStamp fetching in HistoryListener
    
    The attribute fetching in HistoryListener has been improved. It now loops through parent classes until it finds attributes or it reaches the top parent class. This ensures that attributes from extended classes are considered as well.

--- src/History/HistoryListener.php
+++ src/History/HistoryListener.php
@@ -103,12 +103,28 @@ final class HistoryListener
 
     private function isMonitoringDisabled(Envelope $envelope): bool
     {
-        if ($envelope->last(DisableMonitoringStamp::class)) {
-            return true;
+        if ($stamp = $envelope->last(DisableMonitoringStamp::class)) {
+            if ($stamp->onlyWhenNoHandler === false) {
+                return true;
+            }
+
+            return $this->hasNoHandlers($envelope);
         }
 
-        if ((new \ReflectionClass($envelope->getMessage()))->getAttributes(DisableMonitoringStamp::class)) {
-            return true;
+        $reflection = new \ReflectionClass($envelope->getMessage());
+        $attributes = [];
+
+        while ($reflection !== false && $attributes === []) {
+            $attributes = $reflection->getAttributes(DisableMonitoringStamp::class);
+            $reflection = $reflection->getParentClass();
+        }
+
+        if ($attributes !== []) {
+            if ($attributes[0]->newInstance()->onlyWhenNoHandler === false) {
+                return true;
+            }
+
+            return $this->hasNoHandlers($envelope);
         }
 
         return false;
@@ -139,5 +155,10 @@ final class HistoryListener
         }
 
         return new Results($results);
+    }
+
+    private function hasNoHandlers(Envelope $envelope): bool
+    {
+        return $envelope->all(HandledStamp::class) === [];
     }
 }
