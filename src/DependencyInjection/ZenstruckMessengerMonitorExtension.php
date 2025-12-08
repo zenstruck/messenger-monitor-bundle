f4c268112dde0fb88b5f655af91133c9df4c02fc
Christopher Georg <christopher.georg@sr-travel.de>
2025-12-08 09:22:12 +0100

    ci: add tests for PHP 8.5 (replace symfony 7.2.*, 7.3* with 7.4)

--- src/DependencyInjection/ZenstruckMessengerMonitorExtension.php
+++ src/DependencyInjection/ZenstruckMessengerMonitorExtension.php
@@ -27,10 +27,6 @@ use Zenstruck\Messenger\Monitor\History\Model\ProcessedMessage;
  */
 final class ZenstruckMessengerMonitorExtension extends ConfigurableExtension implements ConfigurationInterface
 {
-    /**
-     * TODO can be removed after Symfony 8.0.1 is released
-     * @return TreeBuilder<'array'>
-     */
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $builder = new TreeBuilder('zenstruck_messenger_monitor');
